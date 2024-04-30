72d1bcd945a7e698ea4a55f70784af461102ca2d
garthdp <garthdp4@gmail.com>
2024-04-30 16:57:16 +0200

    changes

--- TaskWavePart1/app/src/main/java/com/example/taskwavepart1/MainActivity.kt
+++ TaskWavePart1/app/src/main/java/com/example/taskwavepart1/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.taskwavepart1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,7 +20,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //Test
-        //Test 2
+        val btnChange : Button = findViewById(R.id.btnChange)
+
+        btnChange.setOnClickListener{
+            val intent = Intent(this, timesheet_entry::class.java)
+            startActivity(intent)
+        }
     }
 }
--- TaskWavePart1/app/src/main/java/com/example/taskwavepart1/timesheet_entry.kt
+++ TaskWavePart1/app/src/main/java/com/example/taskwavepart1/timesheet_entry.kt
@@ -2,21 +2,21 @@ package com.example.taskwavepart1
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class timesheet_entry : AppCompatActivity() {
 
-    val REQUEST_PICK_IMAGE = 1000
 
-    val btnPickImage : Button = findViewById(R.id.btnPickImage)
-    var imageView : ImageView = findViewById(R.id.imageView)
+    @RequiresExtension(extension = Build.VERSION_CODES.R, version = 2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,14 +27,21 @@ class timesheet_entry : AppCompatActivity() {
             insets
         }
 
+        val REQUEST_PICK_IMAGE = 1000
+
+        val btnPickImage : Button = findViewById(R.id.btnPickImage)
+        var imageView : ImageView = findViewById(R.id.imageView)
+
         btnPickImage.setOnClickListener{
             val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
             startActivityForResult(intent, 1)
         }
 
     }
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        var imageView : ImageView = findViewById(R.id.imageView)
         if (resultCode === RESULT_OK) {
             if (requestCode === 1) {
                 val selectedImageUri: Uri = data?.data!!
